0feba58ad25eeef1c646669c9100772be8a974c1
NkululekoNtuli <scelonkululeko2@gmail.com>
2024-12-17 03:01:29 +0200

    using the GUI

---
+++ GUIOutput.java
@@ -0,0 +1,23 @@
+import  javax.swing.JOptionPane;
+
+public class GUIOutput {
+    /**
+     *
+     * @param args
+     */
+    // Javadoc forward slash, double asterisks and asterisks, backslash to
+    // autogenerate documentation  about the program.
+
+    public static  void main(String[] args){
+
+        JOptionPane.showMessageDialog(null, "Revision? I forgot everything!!");
+        //double forward splash is for line comments
+
+        /*
+        forward slash, asterisks and backslash and asterisks
+        is for block comments
+         */
+
+
+    }
+}
